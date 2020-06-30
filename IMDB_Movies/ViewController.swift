e0ed92684afa5f368739a760a6a4508d2eb04ec5
anirudha <anirudhamahale@gmail.com>
2020-06-30 10:24:51 +0530

    Initial Commit

---
+++ IMDB_Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMDB_Movies
+//
+//  Created by Anirudha Mahale on 30/06/20.
+//  Copyright © 2020 Anirudha Mahale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
