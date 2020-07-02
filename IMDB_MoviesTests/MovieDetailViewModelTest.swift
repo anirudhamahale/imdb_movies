cba3da81b8ee90e15d05eae2d2edf25ed3a391ce
anirudha <anirudhamahale@gmail.com>
2020-07-02 17:11:46 +0530

    Added a test to check for failure

--- IMDB_MoviesTests/MovieDetailViewModelTest.swift
+++ IMDB_MoviesTests/MovieDetailViewModelTest.swift
@@ -15,7 +15,7 @@ class MovieDetailViewModelTest: XCTestCase {
   
   override func setUp() {
     // Put setup code here. This method is called before the invocation of each test method in the class.
-    viewModel = MovieDetailViewModel(movieId: 259316, dataProvider: FakeMovieDataProvider())
+    // viewModel = MovieDetailViewModel(movieId: 259316, dataProvider: FakeMovieDataProvider())
   }
   
   override func tearDown() {
@@ -24,7 +24,8 @@ class MovieDetailViewModelTest: XCTestCase {
   }
   
   func testMovieDetails() {
-    let expect = expectation(description: "Fetch movies call should returns movies.")
+    viewModel = MovieDetailViewModel(movieId: 259316, dataProvider: FakeMovieDataProvider())
+    let expect = expectation(description: "Movie details fetched should be same.")
     viewModel.getMovieDetails {
       expect.fulfill()
     }
@@ -35,6 +36,19 @@ class MovieDetailViewModelTest: XCTestCase {
     XCTAssertEqual(viewModel.genres.value, movie.genres)
     XCTAssertEqual(viewModel.date.value, movie.date)
     XCTAssertEqual(viewModel.overview.value, movie.overview)
+    XCTAssertEqual(viewModel.emptyDatastate.value.isDone, true)
+  }
+  
+  func testError() {
+    viewModel = MovieDetailViewModel(movieId: 259317, dataProvider: FakeMovieDataProvider())
+    let expect = expectation(description: "There should be error as movie is not found with the particular id..")
+    viewModel.getMovieDetails {
+      expect.fulfill()
+    }
+    waitForExpectations(timeout: 60, handler: nil)
+    
+    XCTAssertEqual(viewModel.emptyDatastate.value.isFailed, true)
+    
   }
   
 }
